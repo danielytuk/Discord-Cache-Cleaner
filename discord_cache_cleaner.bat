8ecdcb86fa4665db2f0e2df2a02d8adcb2356f5f
Daniel Back <daniel@dytuk.me>
2022-01-01 16:05:15 +0000

    Added Dev Client + Merged PR

--- discord_cache_cleaner.bat
+++ discord_cache_cleaner.bat
@@ -1,13 +1,15 @@
 @echo off
-@title = "Discord Cache Cleaner by danielytuk"
+@title = "Discord Cache Cleaner by danielytuk & Mad_Tom05"
 
 tasklist | find /i "Discord.exe" && taskkill /im Discord.exe /F
 tasklist | find /i "DiscordCanary.exe" && taskkill /im DiscordCanary.exe /F
 tasklist | find /i "DiscordPTB.exe" && taskkill /im DiscordPTB.exe /F
+tasklist | find /i "DiscordDevelopment.exe" && taskkill /im DiscordDevelopment.exe /F
 
 del /f /s /q %AppData%\Discord\Cache\*.*
 del /f /s /q %AppData%\discord\Cache\*.*
 del /f /s /q %AppData%\discordcanary\Cache\*.*
 del /f /s /q %AppData%\discordptb\Cache\*.*
+del /f /s /q %AppData%\DiscordDevelopment\Cache\*.*
 
 exit
